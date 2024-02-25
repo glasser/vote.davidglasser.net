b204a224a281acf4d311923ef1ba4ec177f407bd
David Glasser <glasser@davidglasser.net>
2024-02-24 23:58:30 -0800

    update circleCI

--- ci-install-deps.sh
+++ ci-install-deps.sh
@@ -1,4 +1,4 @@
-HUGO_VERSION=0.17
+HUGO_VERSION=0.122.0
 S3DEPLOY_VERSION=2.3.2
 
 set -x
@@ -7,9 +7,9 @@ set -e
 mkdir tarballs
 cd tarballs
 
-wget "https://github.com/spf13/hugo/releases/download/v${HUGO_VERSION}/hugo_${HUGO_VERSION}_Linux-64bit.tar.gz"
+wget "https://github.com/gohugoio/hugo/releases/download/v${HUGO_VERSION}/hugo_${HUGO_VERSION}_Linux-64bit.tar.gz"
 tar xvzf "hugo_${HUGO_VERSION}_Linux-64bit.tar.gz"
-cp "hugo_${HUGO_VERSION}_linux_amd64/hugo_${HUGO_VERSION}_linux_amd64" ../hugo
+cp hugo ../hugo
 
 wget "https://github.com/bep/s3deploy/releases/download/v${S3DEPLOY_VERSION}/s3deploy_${S3DEPLOY_VERSION}_Linux-64bit.tar.gz"
 tar xzvf "s3deploy_${S3DEPLOY_VERSION}_Linux-64bit.tar.gz"
