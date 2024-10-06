395ac6625fa80cbd048b3d9fe6c670be998973cf
David Glasser <glasser@davidglasser.net>
2024-10-05 22:50:39 -0700

    Install hugo with mise/asdf
    Instead of direnv locally/shell script in CI
    
    This still leaves around direnv for `git subrepo` (which needs
    `PATH=/opt/homebrew/bin:$PATH` while running in order to get the right
    version of bash) and still uses CI-specific shell script to install
    s3deploy. But at least hugo is automated.

--- ci-install-deps.sh
+++ ci-install-deps.sh
@@ -1,4 +1,9 @@
-HUGO_VERSION=0.122.0
+# It would be nice to use mise/asdf for s3deploy but there isn't an asdf plugin
+# for it. I started to write one but got a bit stymied by the fact that the Mac
+# version is now distributed as a pkg rather than a tarball. And we don't really
+# need this outside of CI anyway. So let's just make this CI-specific even if
+# we're installing Hugo with mise.
+
 S3DEPLOY_VERSION=2.3.2
 
 set -x
@@ -6,10 +11,6 @@ set -e
 
 mkdir tarballs
 cd tarballs
-
-wget "https://github.com/gohugoio/hugo/releases/download/v${HUGO_VERSION}/hugo_${HUGO_VERSION}_Linux-64bit.tar.gz"
-tar xvzf "hugo_${HUGO_VERSION}_Linux-64bit.tar.gz"
-cp hugo ../hugo
 
 wget "https://github.com/bep/s3deploy/releases/download/v${S3DEPLOY_VERSION}/s3deploy_${S3DEPLOY_VERSION}_Linux-64bit.tar.gz"
 tar xzvf "s3deploy_${S3DEPLOY_VERSION}_Linux-64bit.tar.gz"
